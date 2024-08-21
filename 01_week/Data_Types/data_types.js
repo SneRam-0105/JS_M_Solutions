adf038c83014f0f38a9273e29a29e1ef6d0b582b
Sneha <133229057+SneRam-0105@users.noreply.github.com>
2024-08-21 16:35:36 +0300

    Week 1 solutions

---
+++ 01_week/Data_Types/data_types.js
@@ -0,0 +1,35 @@
+/*
+- Create a variable named age and assign it an integer value.
+- Create a variable named name and assign it a string value.
+- Create a variable named isStudent and assign it a boolean value.
+- Create a variable named hobbies and assign it an array containing a few string values (e.g., hobbies).
+- Create an info object with two properties: schoolName (a string) and grade (an integer).
+
+- Use console.log() to print each variable to the console.
+
+- Use the typeof operator to check the data type of each variable and print the result using console.log(). */
+
+const age = 25;
+const name = "Sneha";
+const isStudent = true;
+const hobbies = ["badminton", "dance", "movies"];
+
+let info = {
+    schoolName: "Leo",
+    grade: 5,
+}
+
+
+console.log(age);
+console.log(name);
+console.log(isStudent);
+console.log(hobbies);
+console.log(info)
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isStudent);
+console.log(typeof hobbies);
+console.log(typeof info);
+
+
---
+++ 01_week/Operators/operators.js
@@ -0,0 +1,28 @@
+/* 
+- Create variables a and b, and assign them integer values.
+- Use add, subtraction, multiply, divide, and modulus operator for a and b. Store each result in separate variables (sum, difference, product, quotient, reminder).
+
+- Use console.log() to print the results of each operation to the console.
+
+- Add comments in your code to explain what each operation does. */
+
+
+let a = 5;
+let b = 10;
+
+
+let c = a + b;
+let d = a - b;
+let f = a / b;
+let z = a * b;
+let s = a % b;
+
+/* printing the values of all the operations*/
+
+console.log(c);
+console.log(d);
+console.log(f);
+console.log(z);
+console.log(s);
+
+
